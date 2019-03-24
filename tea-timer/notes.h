6cbfcc7ac333a4fd2c97af6467ec0cc4f0e5a03b
Sami Sorell <sami.sorell@iki.fi>
2019-03-24 10:48:47 +0200

    Tea timer complete. Maybe to add more songs in the future..?

---
+++ tea-timer/notes.h
@@ -0,0 +1,24 @@
+#ifndef SS_NOTES_H
+#define SS_NOTES_H
+
+#define C4 262
+#define D4 294
+#define E4 330
+#define F4 349
+#define G4 392
+#define GS4 415
+#define A4 440
+#define AS4 466
+#define B4 494
+#define C5 523
+#define CS5 554
+#define D5 587
+#define DS5 622
+#define E5 659
+#define F5 698
+#define FS5 740
+#define G5 784
+#define GS5 830
+#define A5 880
+
+#endif  // SS_NOTES_H
